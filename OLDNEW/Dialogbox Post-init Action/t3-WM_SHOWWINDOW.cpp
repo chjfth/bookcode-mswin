4ce69946fa66f10059138716012cae352a9d9487
GBchj <chjfth@gmail.com>
2020-05-07 18:04:17 +0800

    .

---
+++ OLDNEW/Dialogbox Post-init Action/t3-WM_SHOWWINDOW.cpp
@@ -0,0 +1,41 @@
+#define STRICT
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
+// In real life, this would be an instance variable
+BOOL g_fShown = FALSE;
+INT_PTR CALLBACK
+DlgProc(HWND hwnd, UINT uiMsg, WPARAM wParam, LPARAM lParam)
+{
+  switch (uiMsg) {
+  case WM_INITDIALOG:
+    return TRUE;
+  
+  case WM_SHOWWINDOW: // wParam==TRUE if the window is being shown
+    if (wParam && !g_fShown) {
+      g_fShown = TRUE;
+      MessageBox(hwnd,
+                 IsWindowVisible(hwnd) ? TEXT("Visible") : TEXT("Not Visible"),
+                 TEXT("t3-WM_SHOWWINDOW"), MB_OK);
+    }
+    break;
+  
+  case WM_CLOSE:
+   EndDialog(hwnd, 0);
+   break;
+  }
+  return FALSE;
+}
+int WINAPI WinMain(HINSTANCE hinst, HINSTANCE hinstPrev,
+                   LPSTR lpCmdLine, int nShowCmd)
+{
+    DialogBox(hinst, MAKEINTRESOURCE(1), NULL, DlgProc);
+    return 0;
+}
+
+/* Compiling commands:
+
+rc box.rc
+cl /Od /Zi t3-WM_SHOWWINDOW.cpp /link /debug user32.lib gdi32.lib box.res 
+
+*/
---
+++ OLDNEW/Dialogbox Post-init Action/t4ok-WM_WINDOWPOSCHANGED.cpp
@@ -0,0 +1,45 @@
+#define STRICT
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
+// In real life, this would be an instance variable
+BOOL g_fShown = FALSE;
+
+INT_PTR CALLBACK
+DlgProc(HWND hwnd, UINT uiMsg, WPARAM wParam, LPARAM lParam)
+{
+  switch (uiMsg) {
+  case WM_INITDIALOG:
+    return TRUE;
+
+  case WM_WINDOWPOSCHANGED:
+    if ((((WINDOWPOS*)lParam)->flags & SWP_SHOWWINDOW) && !g_fShown) 
+    {
+      g_fShown = TRUE;
+      MessageBox(hwnd,
+                 IsWindowVisible(hwnd) ? TEXT("Visible") : TEXT("Not Visible"),
+                 TEXT("t4ok-WM_WINDOWPOSCHANGED"), MB_OK);
+    }
+    break;
+
+  case WM_CLOSE:
+   EndDialog(hwnd, 0);
+   break;
+  }
+  return FALSE;
+}
+
+
+int WINAPI WinMain(HINSTANCE hinst, HINSTANCE hinstPrev,
+                   LPSTR lpCmdLine, int nShowCmd)
+{
+    DialogBox(hinst, MAKEINTRESOURCE(1), NULL, DlgProc);
+    return 0;
+}
+
+/* Compiling commands:
+
+rc box.rc
+cl /Od /Zi t4ok-WM_WINDOWPOSCHANGED.cpp /link /debug user32.lib gdi32.lib box.res 
+
+*/
