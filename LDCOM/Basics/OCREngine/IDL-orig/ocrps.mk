516e6f0e8c62e01fb0102c430939bb58f1b5f5ef
JimmChen vbwin7 <chjfth@gmail.com>
2020-06-07 13:11:36 +0800

    Remove wrongly committed files.

--- LDCOM/Basics/OCREngine/IDL-orig/ocrps.mk
+++
@@ -1,7 +0,0 @@
-ocrps.dll: dlldata.obj ocr_p.obj ocr_i.obj
-  link /dll /out:ocrps.dll /def:ocrps.def \
-  /entry:DllMain dlldata.obj ocr_p.obj ocr_i.obj \
-  kernel32.lib rpcrt4.lib 
-
-.c.obj:
-  cl /c /DWIN32 /D_WIN32_WINNT=0x0500 /DREGISTER_PROXY_DLL $<
