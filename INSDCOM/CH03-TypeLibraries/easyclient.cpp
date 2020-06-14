7fb3778d0ce0ea1757aa44e37298b88993eae6e7
chj Win7Evernote VM <chjfth@gmail.com>
2020-06-14 22:01:31 +0800

    EasyClient.vcxproj ok

--- INSDCOM/CH03-TypeLibraries/easyclient.cpp
+++ INSDCOM/CH03-TypeLibraries/easyclient.cpp
@@ -1,6 +1,17 @@
 // easyclient.cpp
-#import "component\debug\component.dll" no_namespace
-#include <iostream.h>
+
+// #import "component\debug\component.dll" no_namespace // old statement
+// For Chj's VC2010 pattern, it should be modified to:
+#if PlatformName==Win32
+#import "Component\Win32\component.tlb" no_namespace
+#elif PlatformName==x64
+#import "Component\Win32\component.tlb" no_namespace
+#else
+#error Unknown PlatformName, I cannot continue.
+#endif
+
+#include <iostream>
+using namespace std;
 
 void main()
 {
