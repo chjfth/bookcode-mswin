dd54eef7a5618023c688e38880276fcb8ae6e4cd
Jimm Chen from WinXP <chjfth@gmail.com>
2020-06-15 21:42:17 +0800

    Add VJ++ java projects, no success yet.

---
+++ INSDCOM/CH03-TypeLibraries/JavaSum/SumClass.java
@@ -0,0 +1,17 @@
+import sumclasslib.*;
+//
+//
+// SumClass
+//
+//
+public class SumClass implements sumclasslib.ISumClass
+{
+	private static final String CLSID = 
+      "5a870200-7281-11d1-a6fc-0000c0cc7be1";
+
+	public int Sum(int x, int y)
+	{
+		return x + y;
+	}
+}
+
