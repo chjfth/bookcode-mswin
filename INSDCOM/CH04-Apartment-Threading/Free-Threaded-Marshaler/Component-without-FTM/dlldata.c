3135b419ef53723e1cb75fb834798c4b41b1280b
chj Win7Evernote VM <chjfth@gmail.com>
2020-06-26 23:28:01 +0800

    Batch fix as stated in prev commit.

--- INSDCOM/CH04-Apartment-Threading/Free-Threaded-Marshaler/Component-without-FTM/dlldata.c
+++ INSDCOM/CH04-Apartment-Threading/Free-Threaded-Marshaler/Component-without-FTM/dlldata.c
@@ -1,7 +1,7 @@
-#if PlatformName==Win32
+#if defined(_M_IX86)
 # include "Win32\dlldata.c"
-#elif PlatformName==x64
+#elif defined(_M_AMD64)
 # include "x64\dlldata.c"
 #else
-# error This PlatformName is not supported.
+# error No suitable _M_XXX target platform macro detected.
 #endif
