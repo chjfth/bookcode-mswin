209880ae842478191f1d1e0306358629c325aad1
chj Win7Evernote VM <chjfth@gmail.com>
2021-04-20 16:52:34 +0800

    comments

--- PRWIN5/Chap09/BtnLook-About1/PrintWindow.cpp
+++ PRWIN5/Chap09/BtnLook-About1/PrintWindow.cpp
@@ -6,7 +6,9 @@ void fengyuan_DoPrintWindow(HWND hWnd)
 	// Sample code from:
 	// http://www.fengyuan.com/article/wmprint.html
 
+	BOOL b = FALSE;
 	HDC hDCMem = CreateCompatibleDC(NULL);
+	// -- NULL : creates a memory DC compatible with the application's current screen
 	RECT rect;
 	GetWindowRect(hWnd, & rect);
 
@@ -19,15 +21,22 @@ void fengyuan_DoPrintWindow(HWND hWnd)
 
 	HGDIOBJ hOld = SelectObject(hDCMem, hBmp);
 	SendMessage(hWnd, WM_PRINT, (WPARAM) hDCMem, 
-		PRF_CHILDREN | PRF_CLIENT | PRF_ERASEBKGND | PRF_NONCLIENT | PRF_OWNED);
+		PRF_CHILDREN | 
+		PRF_CLIENT | 
+		PRF_ERASEBKGND | 
+		PRF_NONCLIENT | 
+		PRF_OWNED |
+		0);
 
 	SelectObject(hDCMem, hOld);
-	DeleteObject(hDCMem);
+	b = DeleteDC(hDCMem); // DeleteObject(hDCMem);
+	// -- Use DeleteDC() according to MSDN. DeleteObject(hDCMem) returns TRUE as well.
 
-	OpenClipboard(hWnd);
+	b = OpenClipboard(hWnd);
+	b = EmptyClipboard(); 
+	HANDLE clipdata = SetClipboardData(CF_BITMAP, hBmp);
+	// -- Now, we can use Free Clipboard Viewer to view the bitmap in the clipboard.
 
-	EmptyClipboard(); 
-	SetClipboardData(CF_BITMAP, hBmp);
-	CloseClipboard();
+	b = CloseClipboard();
 }
 
