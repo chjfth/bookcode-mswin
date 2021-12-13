3e50a9ebcd7ae75065e301df1e395f820e8bd124
chj Win7Evernote VM <chjfth@gmail.com>
2021-12-13 16:02:23 +0800

    code formatting

--- PRWIN5/Chap17/2-EZTest/EZTest.cpp
+++ PRWIN5/Chap17/2-EZTest/EZTest.cpp
@@ -11,41 +11,43 @@ TCHAR szTitle   [] = TEXT ("EZTest: Test of EZFONT") ;
 
 void PaintRoutine (HWND hwnd, HDC hdc, int cxArea, int cyArea)
 {
-     HFONT      hFont ;
-     int        y, iPointSize ;
-     LOGFONT    lf ;
-     TCHAR      szBuffer [100] ;
-     TEXTMETRIC tm ;
+	HFONT      hFont ;
+	int        y, iPointSize ;
+	LOGFONT    lf ;
+	TCHAR      szBuffer [100] ;
+	TEXTMETRIC tm ;
 
-          // Set Logical Twips mapping mode
+	// Set Logical Twips mapping mode
 
-     SetMapMode (hdc, MM_ANISOTROPIC) ;
-     SetWindowExtEx (hdc, 1440, 1440, NULL) ;
-     SetViewportExtEx (hdc, GetDeviceCaps (hdc, LOGPIXELSX),
-                            GetDeviceCaps (hdc, LOGPIXELSY), NULL) ;
+	SetMapMode (hdc, MM_ANISOTROPIC) ;
+	
+	SetWindowExtEx (hdc, 1440, 1440, NULL) ;
+	SetViewportExtEx (hdc, 
+		GetDeviceCaps (hdc, LOGPIXELSX), GetDeviceCaps (hdc, LOGPIXELSY), 
+		NULL) ;
 
-          // Try some fonts
+	// Try some fonts
 
-     y = 0 ;
+	y = 0 ;
 
-     for (iPointSize = 80 ; iPointSize <= 120 ; iPointSize++)
-     {
-          hFont = EzCreateFont (hdc, TEXT ("Times New Roman"), 
-                                iPointSize, 0, 0, TRUE) ;
+	for (iPointSize = 80 ; iPointSize <= 120 ; iPointSize++)
+	{
+		hFont = EzCreateFont (hdc, TEXT ("Times New Roman"), 
+			iPointSize, 0, 0, TRUE) ;
 
-          GetObject (hFont, sizeof (LOGFONT), &lf) ;
+		GetObject (hFont, sizeof (LOGFONT), &lf) ;
 
-          SelectObject (hdc, hFont) ;
-          GetTextMetrics (hdc, &tm) ;
+		SelectObject (hdc, hFont) ;
+		GetTextMetrics (hdc, &tm) ;
 
-          TextOut (hdc, 0, y, szBuffer, 
-               wsprintf (szBuffer, 
-                         TEXT ("Times New Roman font of %i.%i points, ")
-                         TEXT ("lf.lfHeight = %i, tm.tmHeight = %i"),
-                         iPointSize / 10, iPointSize % 10,
-                         lf.lfHeight, tm.tmHeight)) ;
+		TextOut (hdc, 0, y, szBuffer, 
+			wsprintf (szBuffer, 
+			TEXT ("Times New Roman font of %i.%i points, ")
+			TEXT ("lf.lfHeight = %i, tm.tmHeight = %i"),
+			iPointSize / 10, iPointSize % 10,
+			lf.lfHeight, tm.tmHeight)) ;
 
-          DeleteObject (SelectObject (hdc, GetStockObject (SYSTEM_FONT))) ;
-          y += tm.tmHeight ;
-     }
+		DeleteObject (SelectObject (hdc, GetStockObject (SYSTEM_FONT))) ;
+		y += tm.tmHeight ;
+	}
 }
