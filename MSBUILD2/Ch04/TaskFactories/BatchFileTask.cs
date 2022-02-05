600548a89a9b1050d2080feeae762134317946b3
chjvm-multiVS <chjfth@gmail.com>
2022-02-05 22:57:49 +0800

    minor format

--- MSBUILD2/Ch04/TaskFactories/BatchFileTask.cs
+++ MSBUILD2/Ch04/TaskFactories/BatchFileTask.cs
@@ -7,78 +7,79 @@
     using Microsoft.Build.Framework;
     using Microsoft.Build.Utilities;
 
-public class BatchFileTask : Task
-{
-    public BatchFileTask(string xmlBody)
+    public class BatchFileTask : Task
     {
-        this.InitalizeFromXml(xmlBody);
-    }
-
-    private string Filepath
-    { get; set; }
-
-    public string Message
-    { get; set; }
-
-    public int ExitCode
-    { get; set; }
-
-    public ITaskItem[] Files
-    { get; set; }
-
-    private void InitalizeFromXml(string xmlBody)
-    {
-        if (!string.IsNullOrWhiteSpace(xmlBody))
+        public BatchFileTask(string xmlBody)
         {
-            // parse the doc, should look like this <Script Filepath="..."/>
-            XDocument doc = XDocument.Parse(xmlBody);
-            XNamespace xnamespace = 
-                @"http://schemas.microsoft.com/developer/msbuild/2003";
-            var node = (from n in doc.Elements(xnamespace + "Script")
-                        select n).SingleOrDefault();
-            if (node != null)
-            {
-                this.Filepath = node.Attribute("Filepath").Value;
-            }
+            this.InitalizeFromXml(xmlBody);
         }
-    }
 
-    public override bool Execute()
-    {
-        if (!string.IsNullOrWhiteSpace(Filepath))
+        private string Filepath
+        { get; set; }
+
+        public string Message
+        { get; set; }
+
+        public int ExitCode
+        { get; set; }
+
+        public ITaskItem[] Files
+        { get; set; }
+
+        private void InitalizeFromXml(string xmlBody)
         {
-            // make sure the file exists
-            if (!File.Exists(this.Filepath))
+            if (!string.IsNullOrWhiteSpace(xmlBody))
             {
-                Log.LogError("Batch file not found at [{0}]", this.Filepath);
-            }
-            else
-            {
-                Log.LogMessage(
-                    MessageImportance.High, 
-                    "Executing batch file from [{0}]", 
-                    this.Filepath);
-                string cmdFilepath = ToolLocationHelper.GetPathToSystemFile("cmd.exe");
-                Process process = new Process();
-                process.StartInfo = new ProcessStartInfo(this.Filepath);
-                process.StartInfo.UseShellExecute = true;
-                process.StartInfo.CreateNoWindow = true;
-                process.Start();
-                process.WaitForExit();
-                int exitCode = process.ExitCode;
-                if (exitCode != 0)
+                // parse the doc, should look like this <Script Filepath="..."/>
+                XDocument doc = XDocument.Parse(xmlBody);
+                XNamespace xnamespace = 
+                    @"http://schemas.microsoft.com/developer/msbuild/2003";
+                var node = (from n in doc.Elements(xnamespace + "Script")
+                            select n).SingleOrDefault();
+                if (node != null)
                 {
-                    Log.LogError(
-                        "Non-zero exit code [{0}] from batch file [{1}]",
-                        exitCode,
-                        this.Filepath);
+                    this.Filepath = node.Attribute("Filepath").Value;
                 }
-                // you could set this via a parameter
-                // process.StartInfo.WorkingDirectory
             }
         }
 
-        return !this.Log.HasLoggedErrors;
-    }
-}
+        public override bool Execute()
+        {
+            if (!string.IsNullOrWhiteSpace(Filepath))
+            {
+                // make sure the file exists
+                if (!File.Exists(this.Filepath))
+                {
+                    Log.LogError("Batch file not found at [{0}]", this.Filepath);
+                }
+                else
+                {
+                    Log.LogMessage(
+                        MessageImportance.High, 
+                        "Executing batch file from [{0}]", 
+                        this.Filepath);
+                    string cmdFilepath = ToolLocationHelper.GetPathToSystemFile("cmd.exe");
+                    Process process = new Process();
+                    process.StartInfo = new ProcessStartInfo(this.Filepath);
+                    process.StartInfo.UseShellExecute = true;
+                    process.StartInfo.CreateNoWindow = true;
+                    process.Start();
+                    process.WaitForExit();
+                    int exitCode = process.ExitCode;
+                    if (exitCode != 0)
+                    {
+                        Log.LogError(
+                            "Non-zero exit code [{0}] from batch file [{1}]",
+                            exitCode,
+                            this.Filepath);
+                    }
+                    // you could set this via a parameter
+                    // process.StartInfo.WorkingDirectory
+                }
+            }
+
+            return !this.Log.HasLoggedErrors;
+        }
+
+    } // public class BatchFileTask 
 }
