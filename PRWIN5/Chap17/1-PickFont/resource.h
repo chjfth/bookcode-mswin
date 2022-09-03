c3f1b399ec029c3c43f7bde7b5778b5292baad6d
chj Win7Evernote VM <chjfth@gmail.com>
2022-09-03 19:34:49 +0800

    Adding new dialog from VC2010 IDE.

--- PRWIN5/Chap17/1-PickFont/resource.h
+++ PRWIN5/Chap17/1-PickFont/resource.h
@@ -1,7 +1,8 @@
 //{{NO_DEPENDENCIES}}
-// Microsoft Developer Studio generated include file.
+// Microsoft Visual C++ generated include file.
 // Used by PickFont.rc
 //
+#define IDD_DIALOG1                     103
 #define IDC_LF_HEIGHT                   1000
 #define IDC_LF_WIDTH                    1001
 #define IDC_LF_ESCAPE                   1002
@@ -76,7 +77,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_RESOURCE_VALUE        104
 #define _APS_NEXT_COMMAND_VALUE         40003
 #define _APS_NEXT_CONTROL_VALUE         1067
 #define _APS_NEXT_SYMED_VALUE           101
