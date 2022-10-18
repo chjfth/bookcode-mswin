9cd50aa7794f6334ebe936309b19f41766f1c085
chj Win7Evernote VM <chjfth@gmail.com>
2022-10-18 14:11:49 +0800

    minor code adjust

--- PSSA2000/02-FileCopy/FileCopy.cpp
+++ PSSA2000/02-FileCopy/FileCopy.cpp
@@ -18,44 +18,44 @@ Notices: Copyright (c) 2000 Jeffrey Richter
 // Each I/O Request needs an OVERLAPPED structure and a data buffer
 class CIOReq : public OVERLAPPED {
 public:
-   CIOReq() {
-      Internal = InternalHigh = 0;   
-      Offset = OffsetHigh = 0;   
-      hEvent = NULL;
-      m_nBuffSize = 0;
-      m_pvData = NULL;
-   }
+	CIOReq() {
+		Internal = InternalHigh = 0;   
+		Offset = OffsetHigh = 0;   
+		hEvent = NULL;
+		m_nBuffSize = 0;
+		m_pvData = NULL;
+	}
 
-   ~CIOReq() {
-      if (m_pvData != NULL)
-         VirtualFree(m_pvData, 0, MEM_RELEASE);
-   }
+	~CIOReq() {
+		if (m_pvData != NULL)
+			VirtualFree(m_pvData, 0, MEM_RELEASE);
+	}
 
-   BOOL AllocBuffer(SIZE_T nBuffSize) {
-      m_nBuffSize = nBuffSize;
-      m_pvData = VirtualAlloc(NULL, m_nBuffSize, MEM_COMMIT, PAGE_READWRITE);
-      return(m_pvData != NULL);
-   }
+	BOOL AllocBuffer(SIZE_T nBuffSize) {
+		m_nBuffSize = nBuffSize;
+		m_pvData = VirtualAlloc(NULL, m_nBuffSize, MEM_COMMIT, PAGE_READWRITE);
+		return(m_pvData != NULL);
+	}
 
-   BOOL Read(HANDLE hDevice, PLARGE_INTEGER pliOffset = NULL) {
-      if (pliOffset != NULL) {
-         Offset     = pliOffset->LowPart;
-         OffsetHigh = pliOffset->HighPart;
-      }
-      return(::ReadFile(hDevice, m_pvData, m_nBuffSize, NULL, this));
-   }
+	BOOL Read(HANDLE hDevice, PLARGE_INTEGER pliOffset = NULL) {
+		if (pliOffset != NULL) {
+			Offset     = pliOffset->LowPart;
+			OffsetHigh = pliOffset->HighPart;
+		}
+		return(::ReadFile(hDevice, m_pvData, m_nBuffSize, NULL, this));
+	}
 
-   BOOL Write(HANDLE hDevice, PLARGE_INTEGER pliOffset = NULL) {
-      if (pliOffset != NULL) {
-         Offset     = pliOffset->LowPart;
-         OffsetHigh = pliOffset->HighPart;
-      }
-      return(::WriteFile(hDevice, m_pvData, m_nBuffSize, NULL, this));
-   }
+	BOOL Write(HANDLE hDevice, PLARGE_INTEGER pliOffset = NULL) {
+		if (pliOffset != NULL) {
+			Offset     = pliOffset->LowPart;
+			OffsetHigh = pliOffset->HighPart;
+		}
+		return(::WriteFile(hDevice, m_pvData, m_nBuffSize, NULL, this));
+	}
 
 private:
-   SIZE_T m_nBuffSize;
-   PVOID  m_pvData;
+	SIZE_T m_nBuffSize;
+	PVOID  m_pvData;
 };
 
 
@@ -73,114 +73,127 @@ private:
 
 ///////////////////////////////////////////////////////////////////////////////
 
+BOOL in_FileCopy(PCTSTR pszFileSrc, PCTSTR pszFileDst, LARGE_INTEGER &liFileSizeSrc) 
+{
+	LARGE_INTEGER liFileSizeDst = { 0 };
 
-BOOL FileCopy(PCTSTR pszFileSrc, PCTSTR pszFileDst) {
+	// Open the source file without buffering & get its size
+	CEnsureCloseFile hfileSrc = CreateFile(pszFileSrc, GENERIC_READ, 
+		FILE_SHARE_READ, NULL, OPEN_EXISTING, 
+		FILE_FLAG_NO_BUFFERING | FILE_FLAG_OVERLAPPED, NULL);
+	if (hfileSrc.IsInvalid()) 
+		return FALSE;
 
-   BOOL fOk = FALSE;    // Assume file copy fails
-   LARGE_INTEGER liFileSizeSrc = { 0 }, liFileSizeDst;
+	// Get the file's size
+	GetFileSizeEx(hfileSrc, &liFileSizeSrc);
 
-   try {
-      {
-      // Open the source file without buffering & get its size
-      CEnsureCloseFile hfileSrc = CreateFile(pszFileSrc, GENERIC_READ, 
-         FILE_SHARE_READ, NULL, OPEN_EXISTING, 
-         FILE_FLAG_NO_BUFFERING | FILE_FLAG_OVERLAPPED, NULL);
-      if (hfileSrc.IsInvalid()) goto leave;
+	// Non-buffered I/O requires sector-sized transfers.
+	// I'll use buffer-size transfers since it's easier to calculate.
+	liFileSizeDst.QuadPart = chROUNDUP(liFileSizeSrc.QuadPart, BUFFSIZE);
 
-      // Get the file's size
-      GetFileSizeEx(hfileSrc, &liFileSizeSrc);
+	// Open the destination file without buffering & set its size
+	CEnsureCloseFile hfileDst = CreateFile(pszFileDst, GENERIC_WRITE, 
+		0, NULL, CREATE_ALWAYS, 
+		FILE_FLAG_NO_BUFFERING | FILE_FLAG_OVERLAPPED, hfileSrc);
+	if (hfileDst.IsInvalid()) 
+		return FALSE;
 
-      // Non-buffered I/O requires sector-sized transfers.
-      // I'll use buffer-size transfers since it's easier to calculate.
-      liFileSizeDst.QuadPart = chROUNDUP(liFileSizeSrc.QuadPart, BUFFSIZE);
+	// File systems extend files synchronously. Extend the destination file 
+	// now so that I/Os execute asynchronously improving performance.
+	SetFilePointerEx(hfileDst, liFileSizeDst, NULL, FILE_BEGIN);
+	SetEndOfFile(hfileDst);
 
-      // Open the destination file without buffering & set its size
-      CEnsureCloseFile hfileDst = CreateFile(pszFileDst, GENERIC_WRITE, 
-         0, NULL, CREATE_ALWAYS, 
-         FILE_FLAG_NO_BUFFERING | FILE_FLAG_OVERLAPPED, hfileSrc);
-      if (hfileDst.IsInvalid()) goto leave;
+	// Create an I/O completion port and associate the files with it.
+	CIOCP iocp(0);
+	iocp.AssociateDevice(hfileSrc, CK_READ);  // Read from source file
+	iocp.AssociateDevice(hfileDst, CK_WRITE); // Write to destination file
 
-      // File systems extend files synchronously. Extend the destination file 
-      // now so that I/Os execute asynchronously improving performance.
-      SetFilePointerEx(hfileDst, liFileSizeDst, NULL, FILE_BEGIN);
-      SetEndOfFile(hfileDst);
+	// Initialize record-keeping variables
+	CIOReq ior[MAX_PENDING_IO_REQS];
+	LARGE_INTEGER liNextReadOffset = { 0 };
+	int nReadsInProgress  = 0;
+	int nWritesInProgress = 0;
 
-      // Create an I/O completion port and associate the files with it.
-      CIOCP iocp(0);
-      iocp.AssociateDevice(hfileSrc, CK_READ);  // Read from source file
-      iocp.AssociateDevice(hfileDst, CK_WRITE); // Write to destination file
+	// Prime the file copy engine by simulating that writes have completed.
+	// This causes read operations to be issued.
+	for (int nIOReq = 0; nIOReq < chDIMOF(ior); nIOReq++) 
+	{
 
-      // Initialize record-keeping variables
-      CIOReq ior[MAX_PENDING_IO_REQS];
-      LARGE_INTEGER liNextReadOffset = { 0 };
-      int nReadsInProgress  = 0;
-      int nWritesInProgress = 0;
+		// Each I/O request requires a data buffer for transfers
+		chVERIFY(ior[nIOReq].AllocBuffer(BUFFSIZE));
+		nWritesInProgress++;
+		iocp.PostStatus(CK_WRITE, 0, &ior[nIOReq]);
+	}
 
-      // Prime the file copy engine by simulating that writes have completed.
-      // This causes read operations to be issued.
-      for (int nIOReq = 0; nIOReq < chDIMOF(ior); nIOReq++) {
+	// Loop while outstanding I/O requests still exist
+	while ((nReadsInProgress > 0) || (nWritesInProgress > 0)) 
+	{
 
-         // Each I/O request requires a data buffer for transfers
-         chVERIFY(ior[nIOReq].AllocBuffer(BUFFSIZE));
-         nWritesInProgress++;
-         iocp.PostStatus(CK_WRITE, 0, &ior[nIOReq]);
-      }
+		// Suspend the thread until an I/O completes
+		ULONG_PTR CompKey;
+		DWORD dwNumBytes;
+		CIOReq* pior;
+		iocp.GetStatus(&CompKey, &dwNumBytes, (OVERLAPPED**) &pior, INFINITE);
 
-      // Loop while outstanding I/O requests still exist
-      while ((nReadsInProgress > 0) || (nWritesInProgress > 0)) {
+		switch (CompKey) {
+		case CK_READ:  // Read completed, write to destination
+			nReadsInProgress--;
+			pior->Write(hfileDst);  // Write to same offset read from source
+			nWritesInProgress++;
+			break;
 
-         // Suspend the thread until an I/O completes
-         ULONG_PTR CompKey;
-         DWORD dwNumBytes;
-         CIOReq* pior;
-         iocp.GetStatus(&CompKey, &dwNumBytes, (OVERLAPPED**) &pior, INFINITE);
+		case CK_WRITE: // Write completed, read from source
+			nWritesInProgress--;
+			if (liNextReadOffset.QuadPart < liFileSizeDst.QuadPart) {
+				// Not EOF, read the next block of data from the source file.
+				pior->Read(hfileSrc, &liNextReadOffset);
+				nReadsInProgress++;
+				liNextReadOffset.QuadPart += BUFFSIZE; // Advance source offset
+			}
+			break;
+		}
+	}
 
-         switch (CompKey) {
-         case CK_READ:  // Read completed, write to destination
-            nReadsInProgress--;
-            pior->Write(hfileDst);  // Write to same offset read from source
-            nWritesInProgress++;
-            break;
+	return TRUE;
+}
 
-         case CK_WRITE: // Write completed, read from source
-            nWritesInProgress--;
-            if (liNextReadOffset.QuadPart < liFileSizeDst.QuadPart) {
-               // Not EOF, read the next block of data from the source file.
-               pior->Read(hfileSrc, &liNextReadOffset);
-               nReadsInProgress++;
-               liNextReadOffset.QuadPart += BUFFSIZE; // Advance source offset
-            }
-            break;
-         }
-      }
-      fOk = TRUE;
-      }
-   leave:;
-   }
-   catch (...) {
-   }
+BOOL FileCopy(PCTSTR pszFileSrc, PCTSTR pszFileDst) 
+{
 
-   if (fOk) {
-      // The destination file size is a multiple of the page size. Open the
-      // file WITH buffering to shrink its size to the source file's size.
-      CEnsureCloseFile hfileDst = CreateFile(pszFileDst, GENERIC_WRITE, 
-         0, NULL, OPEN_EXISTING, 0, NULL);
-      if (hfileDst.IsValid()) {
-         
-         SetFilePointerEx(hfileDst, liFileSizeSrc, NULL, FILE_BEGIN);
-         SetEndOfFile(hfileDst);
-      }
-   }
+	BOOL fOk = FALSE;    // Assume file copy fails
+	LARGE_INTEGER liFileSizeSrc = { 0 };
 
-   return(fOk);
+	try 
+	{
+		fOk = in_FileCopy(pszFileSrc, pszFileDst, 
+			liFileSizeSrc // as output
+			);
+	}
+	catch (...) {
+	}
+
+	if (fOk) 
+	{
+		// The destination file size is a multiple of the page size. Open the
+		// file WITH buffering to shrink its size to the source file's size.
+		CEnsureCloseFile hfileDst = CreateFile(pszFileDst, GENERIC_WRITE, 
+			0, NULL, OPEN_EXISTING, 0, NULL);
+		if (hfileDst.IsValid()) {
+
+			SetFilePointerEx(hfileDst, liFileSizeSrc, NULL, FILE_BEGIN);
+			SetEndOfFile(hfileDst);
+		}
+	}
+
+	return(fOk);
 }
 
 
 ///////////////////////////////////////////////////////////////////////////////
 
 
-BOOL Dlg_OnInitDialog(HWND hwnd, HWND hwndFocus, LPARAM lParam) {
-
+BOOL Dlg_OnInitDialog(HWND hwnd, HWND hwndFocus, LPARAM lParam) 
+{
    chSETDLGICONS(hwnd, IDI_FILECOPY);
 
    // Disable Copy button since no file is selected yet.
@@ -192,49 +205,51 @@ BOOL Dlg_OnInitDialog(HWND hwnd, HWND hwndFocus, LPARAM lParam) {
 ///////////////////////////////////////////////////////////////////////////////
 
 
-void Dlg_OnCommand(HWND hwnd, int id, HWND hwndCtl, UINT codeNotify) {
-   
-   TCHAR szPathname[_MAX_PATH];
+void Dlg_OnCommand(HWND hwnd, int id, HWND hwndCtl, UINT codeNotify) 
+{
+	TCHAR szPathname[_MAX_PATH];
 
-   switch (id) {
-   case IDCANCEL:
-      EndDialog(hwnd, id);
-      break;
+	switch (id) {
+	case IDCANCEL:
+		EndDialog(hwnd, id);
+		break;
 
-   case IDOK:
-      // Copy the source file to the destination file.
-      Static_GetText(GetDlgItem(hwnd, IDC_SRCFILE), 
-         szPathname, sizeof(szPathname));
-      SetCursor(LoadCursor(NULL, IDC_WAIT));
-      chMB(FileCopy(szPathname, TEXT("FileCopy.cpy")) 
-         ? "File Copy Successful" : "File Copy Failed");
-      break;
+	case IDOK:
+		// Copy the source file to the destination file.
+		Static_GetText(GetDlgItem(hwnd, IDC_SRCFILE), 
+			szPathname, sizeof(szPathname));
+		SetCursor(LoadCursor(NULL, IDC_WAIT));
+		
+		chMB(FileCopy(szPathname, TEXT("FileCopy.cpy")) 
+			? "File Copy Successful" : "File Copy Failed");
+		
+		break;
 
-   case IDC_PATHNAME:
-      OPENFILENAME ofn = { OPENFILENAME_SIZE_VERSION_400 };
-      ofn.hwndOwner = hwnd;
-      ofn.lpstrFilter = TEXT("*.*\0");
-      lstrcpy(szPathname, TEXT("*.*"));
-      ofn.lpstrFile = szPathname;
-      ofn.nMaxFile = chDIMOF(szPathname);
-      ofn.lpstrTitle = TEXT("Select file to copy");
-      ofn.Flags = OFN_EXPLORER | OFN_FILEMUSTEXIST;
-      BOOL fOk = GetOpenFileName(&ofn);
-      if (fOk) {
-         // Show user the source file's size
-         Static_SetText(GetDlgItem(hwnd, IDC_SRCFILE), szPathname);
-         CEnsureCloseFile hfile = CreateFile(szPathname, 0, 0, NULL, 
-            OPEN_EXISTING, 0, NULL);
-         if (hfile.IsValid()) {
-            LARGE_INTEGER liFileSize;
-            GetFileSizeEx(hfile, &liFileSize);
-            // NOTE: Only shows bottom 32-bits of size
-            SetDlgItemInt(hwnd, IDC_SRCFILESIZE, liFileSize.LowPart, FALSE);
-         }
-      }
-      EnableWindow(GetDlgItem(hwnd, IDOK), fOk);
-      break;
-   }
+	case IDC_PATHNAME:
+		OPENFILENAME ofn = { OPENFILENAME_SIZE_VERSION_400 };
+		ofn.hwndOwner = hwnd;
+		ofn.lpstrFilter = TEXT("*.*\0");
+		lstrcpy(szPathname, TEXT("*.*"));
+		ofn.lpstrFile = szPathname;
+		ofn.nMaxFile = chDIMOF(szPathname);
+		ofn.lpstrTitle = TEXT("Select file to copy");
+		ofn.Flags = OFN_EXPLORER | OFN_FILEMUSTEXIST;
+		BOOL fOk = GetOpenFileName(&ofn);
+		if (fOk) {
+			// Show user the source file's size
+			Static_SetText(GetDlgItem(hwnd, IDC_SRCFILE), szPathname);
+			CEnsureCloseFile hfile = CreateFile(szPathname, 0, 0, NULL, 
+				OPEN_EXISTING, 0, NULL);
+			if (hfile.IsValid()) {
+				LARGE_INTEGER liFileSize;
+				GetFileSizeEx(hfile, &liFileSize);
+				// NOTE: Only shows bottom 32-bits of size
+				SetDlgItemInt(hwnd, IDC_SRCFILESIZE, liFileSize.LowPart, FALSE);
+			}
+		}
+		EnableWindow(GetDlgItem(hwnd, IDOK), fOk);
+		break;
+	}
 }
 
 
@@ -243,11 +258,11 @@ void Dlg_OnCommand(HWND hwnd, int id, HWND hwndCtl, UINT codeNotify) {
 
 INT_PTR WINAPI Dlg_Proc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
 
-   switch (uMsg) {
-   chHANDLE_DLGMSG(hwnd, WM_INITDIALOG, Dlg_OnInitDialog);
-   chHANDLE_DLGMSG(hwnd, WM_COMMAND,    Dlg_OnCommand);
-   }
-   return(FALSE);
+	switch (uMsg) {
+		chHANDLE_DLGMSG(hwnd, WM_INITDIALOG, Dlg_OnInitDialog);
+		chHANDLE_DLGMSG(hwnd, WM_COMMAND,    Dlg_OnCommand);
+	}
+	return(FALSE);
 }
 
 
@@ -256,8 +271,8 @@ INT_PTR WINAPI Dlg_Proc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
 
 int WINAPI _tWinMain(HINSTANCE hinstExe, HINSTANCE, PTSTR pszCmdLine, int) {
 
-   DialogBox(hinstExe, MAKEINTRESOURCE(IDD_FILECOPY), NULL, Dlg_Proc);
-   return(0);
+	DialogBox(hinstExe, MAKEINTRESOURCE(IDD_FILECOPY), NULL, Dlg_Proc);
+	return(0);
 }
 
 
