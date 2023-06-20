9f4221b94475542309ed049f6df8eae150810d0e
chj Win7Evernote VM <chjfth@gmail.com>
2023-06-20 23:42:23 +0800

    23-SEHTerm vcxproj now uses VSPG

--- PAWIN4/23-SEHTerm/SEHTerm.cpp
+++ PAWIN4/23-SEHTerm/SEHTerm.cpp
@@ -3,35 +3,37 @@ Module:  SEHTerm.cpp
 Notices: Copyright (c) 2000 Jeffrey Richter
 ******************************************************************************/
 
-
 #include "..\CmnHdr.h"     /* See Appendix A. */
 #include <tchar.h>
 
-
 ///////////////////////////////////////////////////////////////////////////////
 
+int WINAPI _tWinMain(HINSTANCE hinstExe, HINSTANCE, PTSTR pszCmdLine, int) 
+{
+	__try {
+		int n = MessageBox(NULL, 
+			TEXT("Perform invalid memory access?"), 
+			TEXT("SEHTerm: In try block"), 
+			MB_YESNO);
 
-int WINAPI _tWinMain(HINSTANCE hinstExe, HINSTANCE, PTSTR pszCmdLine, int) {
+		if (n == IDYES) {
+			* (PBYTE) NULL = 5;  // This causes an access violation
+		}
+	}
+	__finally {
+		PCTSTR psz = AbnormalTermination() 
+			? TEXT("MY Abnormal termination !!") 
+			: TEXT("my Normal termination");
 
-   __try {
-      int n = MessageBox(NULL, TEXT("Perform invalid memory access?"), 
-         TEXT("SEHTerm: In try block"), MB_YESNO);
+		MessageBox(NULL, psz, TEXT("SEHTerm: In finally block"), MB_OK);
+	}
 
-      if (n == IDYES) {
-         * (PBYTE) NULL = 5;  // This causes an access violation
-      }
-   }
-   __finally {
-      PCTSTR psz = AbnormalTermination() 
-         ? TEXT("MY Abnormal termination") : TEXT("my Normal termination");
-      MessageBox(NULL, psz, TEXT("SEHTerm: In finally block"), MB_OK);
-   }
+	MessageBox(NULL, 
+		TEXT("Normal process termination"), 
+		TEXT("SEHTerm: After finally block"), 
+		MB_OK);
 
-   MessageBox(NULL, TEXT("Normal process termination"), 
-      TEXT("SEHTerm: After finally block"), MB_OK);
-
-   return(0);
+	return(0);
 }
-
 
 //////////////////////////////// End of File //////////////////////////////////
