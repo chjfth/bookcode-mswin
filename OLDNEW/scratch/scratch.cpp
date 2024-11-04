28d03cfaad546660510523cd32a54b603b88c0cc
chj Win7Evernote VM <chjfth@gmail.com>
2024-11-04 11:23:36 +0800

    Applying AmTemplate to RaymondC's scratch (skeleton) program.

--- OLDNEW/scratch/scratch.cpp
+++ OLDNEW/scratch/scratch.cpp
@@ -15,9 +15,9 @@ HWND g_hwndChild;           /* Optional child window */
 void
 OnSize(HWND hwnd, UINT state, int cx, int cy)
 {
-    if (g_hwndChild) {
-        MoveWindow(g_hwndChild, 0, 0, cx, cy, TRUE);
-    }
+	if (g_hwndChild) {
+		MoveWindow(g_hwndChild, 0, 0, cx, cy, TRUE);
+	}
 }
 
 /*
@@ -28,7 +28,7 @@ OnSize(HWND hwnd, UINT state, int cx, int cy)
 BOOL
 OnCreate(HWND hwnd, LPCREATESTRUCT lpcs)
 {
-    return TRUE;
+	return TRUE;
 }
 
 /*
@@ -39,7 +39,7 @@ OnCreate(HWND hwnd, LPCREATESTRUCT lpcs)
 void
 OnDestroy(HWND hwnd)
 {
-    PostQuitMessage(0);
+	PostQuitMessage(0);
 }
 
 /*
@@ -58,10 +58,10 @@ PaintContent(HWND hwnd, PAINTSTRUCT *pps)
 void
 OnPaint(HWND hwnd)
 {
-    PAINTSTRUCT ps;
-    BeginPaint(hwnd, &ps);
-    PaintContent(hwnd, &ps);
-    EndPaint(hwnd, &ps);
+	PAINTSTRUCT ps;
+	BeginPaint(hwnd, &ps);
+	PaintContent(hwnd, &ps);
+	EndPaint(hwnd, &ps);
 }
 
 /*
@@ -71,10 +71,10 @@ OnPaint(HWND hwnd)
 void
 OnPrintClient(HWND hwnd, HDC hdc)
 {
-    PAINTSTRUCT ps;
-    ps.hdc = hdc;
-    GetClientRect(hwnd, &ps.rcPaint);
-    PaintContent(hwnd, &ps);
+	PAINTSTRUCT ps;
+	ps.hdc = hdc;
+	GetClientRect(hwnd, &ps.rcPaint);
+	PaintContent(hwnd, &ps);
 }
 
 /*
@@ -83,73 +83,73 @@ OnPrintClient(HWND hwnd, HDC hdc)
 LRESULT CALLBACK
 WndProc(HWND hwnd, UINT uiMsg, WPARAM wParam, LPARAM lParam)
 {
-    switch (uiMsg) 
-    {{
-    HANDLE_MSG(hwnd, WM_CREATE, OnCreate);
-    HANDLE_MSG(hwnd, WM_SIZE, OnSize);
-    HANDLE_MSG(hwnd, WM_DESTROY, OnDestroy);
-    HANDLE_MSG(hwnd, WM_PAINT, OnPaint);
-    case WM_PRINTCLIENT: 
-    {
-    	OnPrintClient(hwnd, (HDC)wParam); 
-    	return 0;
-    }
+	switch (uiMsg) 
+	{{
+	HANDLE_MSG(hwnd, WM_CREATE, OnCreate);
+	HANDLE_MSG(hwnd, WM_SIZE, OnSize);
+	HANDLE_MSG(hwnd, WM_DESTROY, OnDestroy);
+	HANDLE_MSG(hwnd, WM_PAINT, OnPaint);
+	case WM_PRINTCLIENT: 
+	{
+		OnPrintClient(hwnd, (HDC)wParam); 
+		return 0;
+	}
 	}}
-    return DefWindowProc(hwnd, uiMsg, wParam, lParam);
+	return DefWindowProc(hwnd, uiMsg, wParam, lParam);
 }
 
 BOOL
 InitApp(void)
 {
-    WNDCLASS wc;
-    wc.style = 0;
-    wc.lpfnWndProc = WndProc;
-    wc.cbClsExtra = 0;
-    wc.cbWndExtra = 0;
-    wc.hInstance = g_hinst;
-    wc.hIcon = NULL;
-    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-    wc.lpszMenuName = NULL;
-    wc.lpszClassName = TEXT("Scratch");
-    
-    if (!RegisterClass(&wc)) 
-    	return FALSE;
-    
+	WNDCLASS wc;
+	wc.style = 0;
+	wc.lpfnWndProc = WndProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = g_hinst;
+	wc.hIcon = NULL;
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wc.lpszMenuName = NULL;
+	wc.lpszClassName = TEXT("Scratch");
+	
+	if (!RegisterClass(&wc)) 
+		return FALSE;
+	
 	InitCommonControls();               /* In case we use a common control */
-    return TRUE;
+	return TRUE;
 }
 
 int WINAPI WinMain(HINSTANCE hinst, HINSTANCE hinstPrev,
-                   LPSTR lpCmdLine, int nShowCmd)
+				   LPSTR lpCmdLine, int nShowCmd)
 {
-    MSG msg;
-    HWND hwnd;
-    g_hinst = hinst;
+	MSG msg;
+	HWND hwnd;
+	g_hinst = hinst;
 
-    if (!InitApp()) 
-    	return 0;
+	if (!InitApp()) 
+		return 0;
 
-    if (SUCCEEDED(CoInitialize(NULL))) {/* In case we use COM */
-        hwnd = CreateWindow(
-            TEXT("Scratch"),                /* Class Name */
-            TEXT("Scratch"),                /* Title */
-            WS_OVERLAPPEDWINDOW,            /* Style */
-            CW_USEDEFAULT, CW_USEDEFAULT,   /* Position */
-            CW_USEDEFAULT, CW_USEDEFAULT,   /* Size */
-            NULL,                           /* Parent */
-            NULL,                           /* No menu */
-            hinst,                          /* Instance */
-            0);                             /* No special parameters */
+	if (SUCCEEDED(CoInitialize(NULL))) {/* In case we use COM */
+		hwnd = CreateWindow(
+			TEXT("Scratch"),                /* Class Name */
+			TEXT("Scratch"),                /* Title */
+			WS_OVERLAPPEDWINDOW,            /* Style */
+			CW_USEDEFAULT, CW_USEDEFAULT,   /* Position */
+			CW_USEDEFAULT, CW_USEDEFAULT,   /* Size */
+			NULL,                           /* Parent */
+			NULL,                           /* No menu */
+			hinst,                          /* Instance */
+			0);                             /* No special parameters */
 
-        ShowWindow(hwnd, nShowCmd);
+		ShowWindow(hwnd, nShowCmd);
 
-        while (GetMessage(&msg, NULL, 0, 0)) {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
-        }
-        CoUninitialize();
-    }
-    return 0;
+		while (GetMessage(&msg, NULL, 0, 0)) {
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		CoUninitialize();
+	}
+	return 0;
 }
 
