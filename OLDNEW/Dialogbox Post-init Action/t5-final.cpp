c483947b66bcd47836ec4e2070c5dc93f0e098aa
chj Win7Evernote VM <chjfth@gmail.com>
2024-11-05 08:38:57 +0800

    Dialogbox Post-init Action: Added .vcxproj/.sln

--- OLDNEW/Dialogbox Post-init Action/t5-final.cpp
+++ OLDNEW/Dialogbox Post-init Action/t5-final.cpp
@@ -5,40 +5,39 @@
 // In real life, this would be an instance variable
 BOOL g_fShown = FALSE;
 
-INT_PTR CALLBACK
-DlgProc(HWND hwnd, UINT uiMsg, WPARAM wParam, LPARAM lParam)
+INT_PTR CALLBACK DlgProc(HWND hwnd, UINT uiMsg, WPARAM wParam, LPARAM lParam)
 {
-  switch (uiMsg) {
-  case WM_INITDIALOG:
-    return TRUE;
-  
-  case WM_WINDOWPOSCHANGED:
-    if ((((WINDOWPOS*)lParam)->flags & SWP_SHOWWINDOW) && !g_fShown) {
-      g_fShown = TRUE;
-      PostMessage(hwnd, WM_APP, 0, 0);
-    }
-    break;
-  
-  case WM_APP:
-      MessageBox(hwnd,
-                 IsWindowVisible(hwnd) ? TEXT("Visible") : TEXT("Not Visible"),
-                 TEXT("test five"), MB_OK);
-      break;
-  
-  case WM_CLOSE:
-   EndDialog(hwnd, 0);
-   break;
-  }
-  return FALSE;
+	switch (uiMsg) {
+	case WM_INITDIALOG:
+		return TRUE;
+
+	case WM_WINDOWPOSCHANGED:
+		if ((((WINDOWPOS*)lParam)->flags & SWP_SHOWWINDOW) && !g_fShown) {
+			g_fShown = TRUE;
+			PostMessage(hwnd, WM_APP, 0, 0);
+		}
+		break;
+
+	case WM_APP:
+		MessageBox(hwnd,
+			IsWindowVisible(hwnd) ? TEXT("Visible") : TEXT("Not Visible"),
+			TEXT("t5-final"), MB_OK);
+		break;
+
+	case WM_CLOSE:
+		EndDialog(hwnd, 0);
+		break;
+	}
+	return FALSE;
 }
 
 
 
 int WINAPI WinMain(HINSTANCE hinst, HINSTANCE hinstPrev,
-                   LPSTR lpCmdLine, int nShowCmd)
+	LPSTR lpCmdLine, int nShowCmd)
 {
-    DialogBox(hinst, MAKEINTRESOURCE(1), NULL, DlgProc);
-    return 0;
+	DialogBox(hinst, MAKEINTRESOURCE(1), NULL, DlgProc);
+	return 0;
 }
 
 /* Compiling commands:
