7358eeaa16833a8b3412acb38cb9de951da043ba
chj Win7Evernote VM <chjfth@gmail.com>
2024-12-19 12:32:18 +0800

    code format

--- PAWIN5/14-VMMap/VMQuery.h
+++ PAWIN5/14-VMMap/VMQuery.h
@@ -5,20 +5,20 @@ Notices: Copyright (c) 2008 Jeffrey Richter & Christophe Nasarre
 
 
 typedef struct {
-   // Region information
-   PVOID  pvRgnBaseAddress;
-   DWORD  dwRgnProtection;  // PAGE_*
-   SIZE_T RgnSize;
-   DWORD  dwRgnStorage;     // MEM_*: Free, Image, Mapped, Private
-   DWORD  dwRgnBlocks;
-   DWORD  dwRgnGuardBlks;   // If > 0, region contains thread stack
-   BOOL   bRgnIsAStack;     // TRUE if region contains thread stack
+	// Region information
+	PVOID  pvRgnBaseAddress;
+	DWORD  dwRgnProtection;  // PAGE_*
+	SIZE_T RgnSize;
+	DWORD  dwRgnStorage;     // MEM_*: Free, Image, Mapped, Private
+	DWORD  dwRgnBlocks;
+	DWORD  dwRgnGuardBlks;   // If > 0, region contains thread stack
+	BOOL   bRgnIsAStack;     // TRUE if region contains thread stack
 
-   // Block information
-   PVOID  pvBlkBaseAddress;
-   DWORD  dwBlkProtection;  // PAGE_*
-   SIZE_T BlkSize;
-   DWORD  dwBlkStorage;     // MEM_*: Free, Reserve, Image, Mapped, Private
+	// Block information
+	PVOID  pvBlkBaseAddress;
+	DWORD  dwBlkProtection;  // PAGE_*
+	SIZE_T BlkSize;
+	DWORD  dwBlkStorage;     // MEM_*: Free, Reserve, Image, Mapped, Private
 } VMQUERY, *PVMQUERY;
 
 
