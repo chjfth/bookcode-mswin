fce84e50027a1399043969f7cb87f3a40d530b59
chj Win7Evernote VM <chjfth@gmail.com>
2025-01-11 11:37:36 +0800

    chj exp code: Add Alt+Tab Icon to Colors3.

--- PRWIN5/Chap11/Colors3/Colors3.cpp
+++ PRWIN5/Chap11/Colors3/Colors3.cpp
@@ -4,11 +4,35 @@
   ----------------------------------------------*/
 
 #include <windows.h>
+#include <windowsx.h>
 #include <commdlg.h>
+
+#include "resource.h"
+
+HINSTANCE g_exeInst;
+HINSTANCE g_dllInst;
+
+UINT_PTR CALLBACK CCHookProc(HWND hdlg, UINT uiMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch(uiMsg)
+	{
+	case WM_INITDIALOG:
+		
+		g_dllInst = GetWindowInstance(hdlg); // (test) it's comdlg32.dll's address
+		
+		HICON hIcon = LoadIcon(g_exeInst, TEXT("MYPROGRAM"));
+		SendMessage(hdlg, WM_SETICON, TRUE, (LPARAM)hIcon);
+		break;
+	}
+	return 0;
+}
+
 
 int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance,
                     PSTR szCmdLine, int iCmdShow)
 {
+	g_exeInst = hInstance;
+
 	static CHOOSECOLOR cc = {};
 	static COLORREF    crCustColors[16] ;
 
@@ -17,9 +41,9 @@ int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance,
 	cc.hInstance      = NULL ;
 	cc.rgbResult      = RGB (0x80, 0x80, 0x80) ;
 	cc.lpCustColors   = crCustColors ;
-	cc.Flags          = CC_RGBINIT | CC_FULLOPEN ;
+	cc.Flags          = CC_RGBINIT | CC_FULLOPEN | CC_ENABLEHOOK ;
 	cc.lCustData      = 0 ;
-	cc.lpfnHook       = NULL ;
+	cc.lpfnHook       = CCHookProc ;
 	cc.lpTemplateName = NULL ;
 
 	BOOL succ = ChooseColor (&cc) ;
