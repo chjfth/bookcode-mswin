106c497853924986baab1d7b5a35c43046606b32
chj Win7Evernote VM <chjfth@gmail.com>
2025-02-06 22:33:38 +0800

    Replace: afxres.h -> windows.h

--- PSSA2000/11-TokenMaster/resource.h
+++ PSSA2000/11-TokenMaster/resource.h
@@ -59,6 +59,8 @@
 #define IDC_TOKENTYPE                   1047
 #define IDB_NODEFAULT                   1048
 
+#define IDC_STATIC (-1)
+
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
