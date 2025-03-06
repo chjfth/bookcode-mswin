d096e1408b99d8e186601dca99d53a3250d40dec
chj Win7Evernote VM <chjfth@gmail.com>
2025-03-06 08:25:39 +0800

    PrintBuf.h: Now use ITC to show ERROR_xxx macro name.

--- PSSA2000/11-TokenMaster/exe_version.h
+++ PSSA2000/11-TokenMaster/exe_version.h
@@ -1,1 +1,1 @@
-#define EXE_VERSION "1.2.9"
+#define EXE_VERSION "1.3.0"
--- PSSA2000/ClassLib/PrintBuf.h
+++ PSSA2000/ClassLib/PrintBuf.h
@@ -22,21 +22,21 @@ Purpose: This class wraps allows sprintf-like operations while automatically
 
 class CPrintBuf {
 public:
-   CPrintBuf(SIZE_T nMaxSizeInBytes = 64 * 1024); // 64KB is default
-   virtual ~CPrintBuf();
+	CPrintBuf(SIZE_T nMaxSizeInBytes = 64 * 1024); // 64KB is default
+	virtual ~CPrintBuf();
 
-   BOOL Print(PCTSTR pszFmt, ...);
-   BOOL PrintError(DWORD dwError = GetLastError());
-   operator PCTSTR() { return(m_pszBuffer); }
-   void Clear();
-
-private:
-   LONG Filter(EXCEPTION_POINTERS* pep);
+	BOOL Print(PCTSTR pszFmt, ...);
+	BOOL PrintError(DWORD dwError = GetLastError());
+	operator PCTSTR() { return(m_pszBuffer); }
+	void Clear();
 
 private:
-   int   m_nMaxSizeInBytes;
-   int   m_nCurSize;    // In number of characters
-   PTSTR m_pszBuffer;
+	LONG Filter(EXCEPTION_POINTERS* pep);
+
+private:
+	int   m_nMaxSizeInBytes;
+	int   m_nCurSize;    // In number of characters
+	PTSTR m_pszBuffer;
 };
 
 
@@ -45,20 +45,21 @@ private:
 
 #ifdef PRINTBUF_IMPL
 
+#include <mswin/WinError.itc.h>
 
 ///////////////////////////////////////////////////////////////////////////////
 
 
 CPrintBuf::CPrintBuf(SIZE_T nMaxSizeInBytes) {
 
-   // This constructor sets initial values of members, and reserves a block
-   // of addresses of size nMaxSizeInBytes and commits a single page.
-   m_nMaxSizeInBytes = (int)nMaxSizeInBytes;
-   m_nCurSize = 0;
-   m_pszBuffer = (PTSTR) 
-      VirtualAlloc(NULL, m_nMaxSizeInBytes, MEM_RESERVE, PAGE_READWRITE);
-   chASSERT(m_pszBuffer != NULL);
-   chVERIFY(VirtualAlloc(m_pszBuffer, 1, MEM_COMMIT, PAGE_READWRITE) != NULL);
+	// This constructor sets initial values of members, and reserves a block
+	// of addresses of size nMaxSizeInBytes and commits a single page.
+	m_nMaxSizeInBytes = (int)nMaxSizeInBytes;
+	m_nCurSize = 0;
+	m_pszBuffer = (PTSTR) 
+		VirtualAlloc(NULL, m_nMaxSizeInBytes, MEM_RESERVE, PAGE_READWRITE);
+	chASSERT(m_pszBuffer != NULL);
+	chVERIFY(VirtualAlloc(m_pszBuffer, 1, MEM_COMMIT, PAGE_READWRITE) != NULL);
 }
 
 
@@ -67,7 +68,7 @@ CPrintBuf::CPrintBuf(SIZE_T nMaxSizeInBytes) {
 
 CPrintBuf::~CPrintBuf() {
 
-   VirtualFree(m_pszBuffer, 0, MEM_RELEASE);
+	VirtualFree(m_pszBuffer, 0, MEM_RELEASE);
 }
 
 
@@ -76,9 +77,9 @@ CPrintBuf::~CPrintBuf() {
 
 void CPrintBuf::Clear() {
 
-   VirtualFree(m_pszBuffer, m_nMaxSizeInBytes, MEM_DECOMMIT);
-   chVERIFY(VirtualAlloc(m_pszBuffer, 1, MEM_COMMIT, PAGE_READWRITE) != NULL);
-   m_nCurSize = 0;
+	VirtualFree(m_pszBuffer, m_nMaxSizeInBytes, MEM_DECOMMIT);
+	chVERIFY(VirtualAlloc(m_pszBuffer, 1, MEM_COMMIT, PAGE_READWRITE) != NULL);
+	m_nCurSize = 0;
 }
 
 
@@ -147,15 +148,17 @@ int CPrintBuf::Print(PCTSTR pszFmt , ...)
 
 BOOL CPrintBuf::PrintError(DWORD dwErr) {
 
-   // Append the last error string text to the buffer.
-   PTSTR pszMsg = NULL;
-   BOOL fOk = (0 != FormatMessage(
-      FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM, NULL, 
-      dwErr, 0, (PTSTR) &pszMsg, 0, NULL));
-   fOk = fOk && (Print(TEXT("Error %d: %s"), dwErr, pszMsg) >= 0);
-   if (pszMsg != NULL)
-      LocalFree(pszMsg);
-   return(fOk);
+	// Append the last error string text to the buffer.
+	PTSTR pszMsg = NULL;
+	BOOL fOk = (0 != FormatMessage(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM, NULL, 
+		dwErr, 0, (PTSTR) &pszMsg, 0, NULL));
+	
+	fOk = fOk && (Print(TEXT("Error %d(%s): %s"), dwErr, ITCS(dwErr, itc::WinError), pszMsg) >= 0);
+	
+	if (pszMsg != NULL)
+		LocalFree(pszMsg);
+	return(fOk);
 }
 
 
