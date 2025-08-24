3375d05e1d890ba99f99ff60ea1ae22245cbc998
chj <chjfth@gmail.com>
2025-08-24 22:37:36 +0800

    minor

--- INDX98/Chap05/SetModeEx/SetModeEx.cpp
+++ INDX98/Chap05/SetModeEx/SetModeEx.cpp
@@ -1,5 +1,6 @@
 #define WIN32_LEAN_AND_MEAN
-// You must include this define to use QueryInterface
+
+// You must include this define to use QueryInterface(IID_IDirectDraw2)
 #define INITGUID
 
 #include <windows.h>
