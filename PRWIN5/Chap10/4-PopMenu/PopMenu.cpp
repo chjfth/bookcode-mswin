40204cfb677ed8598acc7009987880739d2a59bf
chj Win7Evernote VM <chjfth@gmail.com>
2026-08-25 14:27:30 +0800

    PRWIN5: Add Chap10 PopMenu

--- PRWIN5/Chap10/4-PopMenu/PopMenu.cpp
+++ PRWIN5/Chap10/4-PopMenu/PopMenu.cpp
@@ -1,17 +1,15 @@
-/*-----------------------------------------
-   MENUDEMO.C -- Menu Demonstration
-                 (c) Charles Petzold, 1998
-  -----------------------------------------*/
+/*----------------------------------------
+   POPMENU.C -- Popup Menu Demonstration
+                (c) Charles Petzold, 1998
+  ----------------------------------------*/
 
 #include <windows.h>
-
 #include "resource.h"
-
-#define ID_TIMER 1
 
 LRESULT CALLBACK WndProc (HWND, UINT, WPARAM, LPARAM) ;
 
-TCHAR szAppName[] = TEXT ("MenuDemo") ;
+HINSTANCE hInst ;
+TCHAR     szAppName[] = TEXT ("PopMenu") ;
 
 int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance,
 	PSTR szCmdLine, int iCmdShow)
@@ -25,10 +23,10 @@ int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance,
 	wndclass.cbClsExtra    = 0 ;
 	wndclass.cbWndExtra    = 0 ;
 	wndclass.hInstance     = hInstance ;
-	wndclass.hIcon         = LoadIcon (hInstance, MAKEINTRESOURCE(1));
+	wndclass.hIcon         = LoadIcon (hInstance, MAKEINTRESOURCE(1)) ;
 	wndclass.hCursor       = LoadCursor (NULL, IDC_ARROW) ;
 	wndclass.hbrBackground = (HBRUSH) GetStockObject (WHITE_BRUSH) ;
-	wndclass.lpszMenuName  = szAppName ;
+	wndclass.lpszMenuName  = NULL ;
 	wndclass.lpszClassName = szAppName ;
 
 	if (!RegisterClass (&wndclass))
@@ -38,7 +36,9 @@ int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance,
 		return 0 ;
 	}
 
-	hwnd = CreateWindow (szAppName, TEXT ("Menu Demonstration"),
+	hInst = hInstance ;
+
+	hwnd = CreateWindow (szAppName, TEXT ("Popup Menu Demonstration"),
 		WS_OVERLAPPEDWINDOW,
 		CW_USEDEFAULT, CW_USEDEFAULT,
 		400, 300, // width, height
@@ -57,30 +57,37 @@ int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance,
 
 LRESULT CALLBACK WndProc (HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-	static int idColor [5] = { 
+	static HMENU hMenu ;
+	static int   idColor [5] = { 
 		WHITE_BRUSH, LTGRAY_BRUSH, GRAY_BRUSH, DKGRAY_BRUSH, BLACK_BRUSH 
 	} ;
-	static int iSelection = IDM_BKGND_WHITE ;
-	HMENU      hMenu ;
+	static int   iSelection = IDM_BKGND_WHITE ;
+	POINT        point ;
 
 	switch (message)
 	{
-	case WM_COMMAND:
-		hMenu = GetMenu (hwnd) ;
+	case WM_CREATE:
+		hMenu = LoadMenu (hInst, szAppName) ;
+		hMenu = GetSubMenu (hMenu, 0) ;
+		return 0 ;
 
+	case WM_RBUTTONUP:
+		point.x = LOWORD (lParam) ;
+		point.y = HIWORD (lParam) ;
+		ClientToScreen (hwnd, &point) ;
+
+		// Pop-up our menu:
+		TrackPopupMenu (hMenu, TPM_RIGHTBUTTON, point.x, point.y, 0, hwnd, NULL) ;
+
+		return 0 ;
+
+	case WM_COMMAND:
 		switch (LOWORD (wParam))
 		{
 		case IDM_FILE_NEW:
 		case IDM_FILE_OPEN:
 		case IDM_FILE_SAVE:
 		case IDM_FILE_SAVE_AS:
-			MessageBeep (0) ;
-			return 0 ;
-
-		case IDM_APP_EXIT:
-			SendMessage (hwnd, WM_CLOSE, 0, 0) ;
-			return 0 ;
-
 		case IDM_EDIT_UNDO:
 		case IDM_EDIT_CUT:
 		case IDM_EDIT_COPY:
@@ -106,36 +113,22 @@ LRESULT CALLBACK WndProc (HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 			InvalidateRect (hwnd, NULL, TRUE) ;
 			return 0 ;
 
-		case IDM_TIMER_START:
-			if (SetTimer (hwnd, ID_TIMER, 1000, NULL))
-			{
-				EnableMenuItem (hMenu, IDM_TIMER_START, MF_GRAYED) ;
-				EnableMenuItem (hMenu, IDM_TIMER_STOP,  MF_ENABLED) ;
-			}
+		case IDM_APP_ABOUT:
+			MessageBox (hwnd, TEXT ("Popup Menu Demonstration Program\n")
+				TEXT ("(c) Charles Petzold, 1998"),
+				szAppName, MB_ICONINFORMATION | MB_OK) ;
 			return 0 ;
 
-		case IDM_TIMER_STOP:
-			KillTimer (hwnd, ID_TIMER) ;
-			EnableMenuItem (hMenu, IDM_TIMER_START, MF_ENABLED) ;
-			EnableMenuItem (hMenu, IDM_TIMER_STOP,  MF_GRAYED) ;
+		case IDM_APP_EXIT:
+			SendMessage (hwnd, WM_CLOSE, 0, 0) ;
 			return 0 ;
 
 		case IDM_APP_HELP:
 			MessageBox (hwnd, TEXT ("Help not yet implemented!"),
 				szAppName, MB_ICONEXCLAMATION | MB_OK) ;
 			return 0 ;
-
-		case IDM_APP_ABOUT:
-			MessageBox (hwnd, TEXT ("Menu Demonstration Program\n")
-				TEXT ("(c) Charles Petzold, 1998"),
-				szAppName, MB_ICONINFORMATION | MB_OK) ;
-			return 0 ;
 		}
 		break ;
-
-	case WM_TIMER:
-		MessageBeep (0) ;
-		return 0 ;
 
 	case WM_DESTROY:
 		PostQuitMessage (0) ;
